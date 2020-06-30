325a064a5ab0bbc05377a279a3c3ff059b38a2df
Karim Wölbert <karimwolbert@MacBook-Pro.fritz.box>
2020-06-30 18:07:32 +0200

    added graphics and sounds

--- Falling_Project/Falling.py
+++ Falling_Project/Falling.py
@@ -1,28 +1,69 @@
 # Simple Falling Skies Project with Turtle
 
 import turtle
+import random
+import playsound
+
+score = 0
+lives = 3 
 
 window = turtle.Screen() # creating Screen
 window.title('Falling Skies') # creating title
 window.bgcolor('blue') # creating backgroundcolor
+window.bgpic('africa_background.gif') #implementing background picture for much more realistic immersion
 window.setup(width=800, height=600)
 window.tracer(0) # screen does not update until you activate the update method
 
+window.register_shape('Lion-in-a-Box.gif')
+window.register_shape('meat.gif')
+window.register_shape('hunter.gif')
 
 player = turtle.Turtle() # adding the player
 player.speed(0) # With this command its drawing as fast as its can. Hs to be the first command before its drawing
-player.shape('square')
+player.shape('Lion-in-a-Box.gif')
 player.color('white')
 player.penup() # turtle stops drawing lines. Has to be before the goto command
 player.goto(0, -250) # in turtle 0, 0 is the center so in this case the player starts at the bottom center
 player.direction = 'stop' # player does not move withoput any commands at the start
 
-good_guy = turtle.Turtle() # adding the player
-good_guy.speed(0) # With this command its drawing as fast as its can. Hs to be the first command before its drawing
-good_guy.shape('circle')
-good_guy.color('black')
-good_guy.penup() # turtle stops drawing lines. Has to be before the goto command
-good_guy.goto(0, 250) # in turtle 0, 0 is the center so in this case the player starts at the bottom center
+# Create a list of good guys
+good_guys = []
+
+# Add the good guys to the list
+for _ in range(20):
+    good_guy = turtle.Turtle() # adding the player
+    good_guy.speed(0) # With this command its drawing as fast as its can. Hs to be the first command before its drawing
+    good_guy.shape('meat.gif') # adding meat picture 
+    good_guy.color('black')
+    good_guy.penup() # turtle stops drawing lines. Has to be before the goto command
+    good_guy.goto(-100, 250) # in turtle 0, 0 is the center so in this case the player starts at the bottom center
+    good_guy.speed = random.randint(1, 4)
+    good_guys.append(good_guy)
+
+
+# Create a list of bad guys
+bad_guys = []
+
+# Add the good guys to the list
+for _ in range(20):
+    bad_guy = turtle.Turtle() # adding the player
+    bad_guy.speed(0) # With this command its drawing as fast as its can. Hs to be the first command before its drawing
+    bad_guy.shape('hunter.gif')
+    bad_guy.color('red')
+    bad_guy.penup() # turtle stops drawing lines. Has to be before the goto command
+    bad_guy.goto(100, 250) # in turtle 0, 0 is the center so in this case the player starts at the bottom center
+    bad_guy.speed = random.randint(1, 4)
+    bad_guys.append(bad_guy)
+
+# Make the scoreboard
+pen = turtle.Turtle() # adding the player
+pen.hideturtle()
+pen.speed(0) # With this command its drawing as fast as its can. Hs to be the first command before its drawing
+pen.shape('circle')
+pen.color('black')
+pen.penup() # turtle stops drawing lines. Has to be before the goto command
+pen.goto(0, 260)
+pen.write('Score: {} Lives: {}'.format(score, lives), align='center', font=('courier', 24, 'normal')) 
 
 
 # Functions
@@ -52,9 +93,49 @@ while True:
         x += 3 
         player.setx(x)
     
-    # Move the good guy
- 
-    
-    
+    # Move the good guys
+    for good_guy in good_guys:
+        y = good_guy.ycor()
+        y -= good_guy.speed
+        good_guy.sety(y)
+
+        # Check if off screen
+        if y < -300:
+            x = random.randint(-380, 380)
+            y = random.randint(300, 400)
+            good_guy.goto(x, y)
+
+
+        # Check for collision with the player
+        if good_guy.distance(player) < 40:
+            playsound.playsound('Roaring.wav', False)
+            x = random.randint(-380, 380)
+            y = random.randint(300, 400)
+            good_guy.goto(x, y)
+            score += 1
+            pen.clear()
+            pen.write('Score: {} Lives: {}'.format(score, lives), align='center', font=('courier', 24, 'normal')) 
+
+    # Move the bad guys
+    for bad_guy in bad_guys:
+        y = bad_guy.ycor()
+        y -= bad_guy.speed
+        bad_guy.sety(y)
+
+        # Check if off screen
+        if y < -300:
+            x = random.randint(-380, 380)
+            y = random.randint(300, 400)
+            bad_guy.goto(x, y)
+
+
+        # Check for collision with the player
+        if bad_guy.distance(player) < 40:
+            x = random.randint(-380, 380)
+            y = random.randint(300, 400)
+            bad_guy.goto(x, y)            
+            lives -= 1
+            pen.clear()
+            pen.write('Score: {} Lives: {}'.format(score, lives), align='center', font=('courier', 24, 'normal')) 
 
 window.mainloop()
