93b9e85b13aead2a12f54f7ba6f1350321a42eda
Karim Wölbert <karimwolbert@MacBook-Pro.fritz.box>
2020-07-14 15:02:13 +0200

    added rotation method and player class

--- Space_Project/space.py
+++ Space_Project/space.py
@@ -28,19 +28,43 @@ class Sprite(): # the class for objects like player, enemy etc.
         self.color = color
         self.dx = 0 # horizontal speed (if dx is positiv the object moves to the right, if negative left)
         self.dy = 0 # vertical speed (if dy is positiv the object moves up, if negative down)
+        self.heading = 0
+        self.da = 0
     
     def update(self):
         self.x += self.dx
         self.y += self.dy
+        self.heading += self.da # the heading is gonna change by the delta of the angle
     
     def render(self, pen):
         pen.goto(self.x, self.y)
+        pen.setheading(self.heading) # sets the orientation of the turle to east
         pen.shape(self.shape)
         pen.color(self.color)
         pen.stamp() # puts the pen on the screen
 
+class Player(Sprite): # inherets the attributes from the parent class
+    def __init__(self, x, y, shape, color):
+        Sprite.__init__(self, 0, 0, shape, color)
+        self.lives = 3 # only the player has lives and score
+        self.score = 0
+        self.heading = 90 #player goes up
+        self.da = 0 # changing the angle
+    
+    def rotate_left(self):
+        self.da = 5
+
+    def rotate_right(self):
+        self.da = -5
+
+    def stop_rotation(self):
+        self.da = 0
+    
+
+
+
 # Creating the player sprite as a spaceship
-player = Sprite(0,0, 'triangle', 'white') # putting the player in the center
+player = Player(0,0, 'triangle', 'white') # putting the player in the center
 player.dx = 1
 player.dy = 0.5
 
@@ -58,6 +82,13 @@ sprites.append(player)
 sprites.append(enemy)
 sprites.append(powerup)
 
+# Keyboard bindings
+window.listen()
+window.onkeypress(player.rotate_left, 'Left')
+window.onkeypress(player.rotate_right, 'Right')
+
+window.onkeyrelease(player.stop_rotation, 'Left') # the moment you dont press left or right it will stop rotating!
+window.onkeyrelease(player.stop_rotation, 'Right')
 
 # Main Loop
 while True:
